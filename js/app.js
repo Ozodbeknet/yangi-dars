f7ff90927c2c29aad25db62d09954f65cacc968b
Ozodbek <Ozodbek@gmail.com>
2025-07-19 10:21:37 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,87 @@
+
+// 2.
+// let son = +prompt("Haqiqiy son kiriting: ");
+
+// console.log(Math.floor(son));
+// console.log(Math.ceil(son));
+// console.log(Math.round(son));
+
+// 2. Random son topish
+
+
+// let random = Math.random() * 10 + 1;
+// console.log(Math.trunc(random));
+
+
+// let a;
+// let b = null;
+
+// console.log(a, "O'zgaruvchi bor lekin unga qiymat berilmagan");
+// console.log(b, "O'zgaruvchi mavjud lekin uning qimati yo'qligi uchun null");
+
+
+// count = 0;
+// while (count < 10) {
+//   let son = Math.floor(Math.random() * 100 + 1);
+//   count += 1;
+//   console.log(son);
+// }
+
+
+// let son = +prompt("Sonni kiriting: ");
+// console.log(Math.pow(son, 2));
+
+
+// let son = 1;
+// while (son < 19) {
+//   if (son % 2 !== 0) {
+//     son++;
+//   }
+//   son += 1;
+//   console.log(son);
+// }
+
+
+
+// let random = Math.floor(Math.random() * 10) + 1;
+// do {
+//   son = +prompt("1 dan 10 gacha son kiriting:");
+// } while (son !== random);
+
+// alert(random);
+
+
+// let son = 1;
+
+// while (son < 50) {
+//   if (son === 21) {
+//     break;
+//   }
+//   if (son % 3 === 0) {
+//     console.log(son);
+//   }
+//   son++;
+// }
+
+
+// let sanoq = 0;
+// let kattasi;
+// while (sanoq < 5) {
+//   let random = Math.floor(Math.random() * 100) + 1;
+//   console.log(random);
+//   if (sanoq === 0) {
+//     kattasi = random;
+//   } else {
+//     kattasi = Math.max(kattasi, random);
+//   }
+
+//   sanoq++;
+// }
+// console.log("Kattsi", kattasi);
+
+// let son;
+
+// while (son === undefined || son === null || isNaN(son)) {
+//   son = +prompt("Son kiriting:");
+// }
+// console.log(son);
